d96545e44cccf0ff1d32978fa5532615a6531727
Angie Lorena Ibarguen <aibarguenmazo@gmail.com>
2017-11-21 07:57:52 -0500

    agregando etiqueta p una h2 y un input

--- ejercicio1.php
+++ ejercicio1.php
@@ -8,7 +8,10 @@
 <body>
 
 	<h2>Aprendiendo GIT</h2>
-	
+	<p>QUE ES GIT?</p>
 
+	<h1>Git, es un software de control de versiones diseñado por Linus Torvalds. La pregunta es ¿qué es control de versiones? Pues bien, se define como control de versiones a la gestión de los diversos cambios que se realizan sobre los elementos de algún producto o una configuración del mismo es decir a la gestión de los diversos cambios que se realizan sobre los elementos de algún producto o una configuración, y para los que aún no les queda claro del todo, control de versiones es lo que se hace al momento de estar desarrollando un software o una página web.</h1>
+
+<input type="submit" name="Siguiente">
 </body>
 </html>
